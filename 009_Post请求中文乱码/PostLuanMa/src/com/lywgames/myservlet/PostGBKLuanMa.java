dbe5e776958c8800e04b9d2414491c92b7b07415
liyangwei999 <1611086176@qq.com>
2019-09-16 16:19:07 +0800

    009_Post请求中文乱码

---
+++ 009_Post请求中文乱码/PostLuanMa/src/com/lywgames/myservlet/PostGBKLuanMa.java
@@ -0,0 +1,27 @@
+package com.lywgames.myservlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PostGBKLuanMa extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 设置请求体里面的文字编码, 需要写在获取数据之前。
+		req.setCharacterEncoding("GBK");
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		System.out.println("username = " + username + " password = " + password);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
